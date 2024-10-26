593c57112c918c60a01f9f5a8b63c5802c5daa07
たくや <takuyadayo014@gmail.com>
2024-10-26 11:14:34 +0900

    fix: 表示されなかったので修正

--- web_sample/app.py
+++ web_sample/app.py
@@ -11,11 +11,6 @@ def home():
 def voice_recognition():
     data = request.get_json()
     print(data['result'])
-    if data['result'].find("テイクオフ") != -1:
-        print('テイクオフ')
-    else:
-        print('着陸')
-    return 'success'
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port="6000")
+    app.run(debug=True)
