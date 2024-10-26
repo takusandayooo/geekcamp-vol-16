cfd5a85db75aab63aa6273ba1138aded75e3dfbe
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-10-26 17:53:34 +0900

    feat: グループ分けのエントリーポイントを実装

--- main/app.py
+++ main/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request
+from dotenv import load_dotenv
+from flask import Flask, abort, jsonify, render_template, request
+from pydantic import ValidationError
+
+from group_split.prompt import Groups, Introductions, split_groups
 
 
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -14,9 +19,21 @@ def follow_up():
 
     return render_template("follow_up.html")
 
-@app.route('/group_split', methods=['GET'])
-def voice_recognition():
-    return render_template('group_split.html')
+@app.route('/group_split', methods=['GET', 'POST'])
+def group_split():
+    if request.method == 'GET':
+        return render_template('group_split.html')
+    else:
+        try:
+            data = request.get_json()
+            intros = Introductions(**data["intros"])
+        except ValidationError:
+            abort(500, description="原因不明のエラーが発生しました。時間を空けて実行して下さい。")
+
+        groups: Groups | None = split_groups(intros)
+        if groups is None:
+            abort(500, description="原因不明のエラーが発生しました。時間を空けて実行して下さい。")
+        return jsonify(groups.model_dump())
 
 
 if __name__ == '__main__':
